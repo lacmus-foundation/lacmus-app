9d57211b3b7c9f22ba3ed6b8f31f228332f31413
gosha20777 <gosha20777@live.ru>
2021-08-03 11:03:20 +0300

    build with net 5.0

--- mk-release.sh
+++ mk-release.sh
@@ -3,11 +3,11 @@ rm -rf ./bin/app/ && rm -rf ./src/bin/Release/
 echo "restoring packeges"
 dotnet restore
 echo -n "building for linux"
-dotnet publish --framework netcoreapp3.1 --runtime="linux-x64" -c Release -o ./bin/app/linux
+dotnet publish --framework net5.0 --runtime="linux-x64" -c Release -o ./bin/app/linux
 echo -n "building for win10"
-dotnet publish --framework netcoreapp3.1 --runtime="win10-x64" -c Release -o ./bin/app/win10
+dotnet publish --framework net5.0 --runtime="win10-x64" -c Release -o ./bin/app/win10
 echo -n "building for osx"
-dotnet publish --framework netcoreapp3.1 --runtime="osx-x64" -c Release -o ./bin/app/osx
+dotnet publish --framework net5.9 --runtime="osx-x64" -c Release -o ./bin/app/osx
 cd ./bin/app/
 zip -r -9 ./linux.zip ./linux/
 zip -r -9 ./win10.zip ./win10/
