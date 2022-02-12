f8459266ba1fcc259eb2aeb92173f0ad27035cfa
gosha20777 <gosha20777@live.ru>
2022-02-13 01:12:15 +0300

    fix tests & build script:

--- mk-release.sh
+++ mk-release.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-rm -rf ./bin/app/ && rm -rf ./src/bin/Release/
+rm -rf ./bin/app/ && \
+    rm -rf ./src/LacmusApp/bin/Release/ && \
+    rm -rf ./src/LacmusApp.Avalonia/bin/Release/
 echo "restoring packeges"
 dotnet restore src/LacmusApp.sln
 echo -n "building for linux"
 dotnet publish --framework net5.0 --runtime="linux-x64" -c Release -o ./bin/app/linux src/LacmusApp.sln
+mv ./bin/app/linux/LacmusApp.Avalonia ./bin/app/linux/LacmusApp
 echo -n "building for win10"
 dotnet publish --framework net5.0 --runtime="win10-x64" -c Release -o ./bin/app/win10 src/LacmusApp.sln
+mv ./bin/app/win10/LacmusApp.Avalonia.exe ./bin/app/win10/LacmusApp.exe
 echo -n "building for osx"
 dotnet publish --framework net5.0 --runtime="osx-x64" -c Release -o ./bin/app/osx src/LacmusApp.sln
+mv ./bin/app/osx/LacmusApp.Avalonia ./bin/app/osx/LacmusApp
 cd ./bin/app/
 zip -r -9 ./linux.zip ./linux/
 zip -r -9 ./win10.zip ./win10/
